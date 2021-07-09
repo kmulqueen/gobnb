f820120c932b3940b878ebd3def9fb7997dc46d1
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-07-09 13:45:59 -0600

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/kmulqueen/gobnb/pkg/config"
+	"github.com/kmulqueen/gobnb/pkg/handlers"
+	"github.com/kmulqueen/gobnb/pkg/render"
+)
+
+// Variables available to entire main package
+const port = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+
+	// Change to true when in production
+	app.InProduction = false
+
+	// Initialize a new session & set the lifetime to 24 hours
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true // Should session persist after browser window closed?
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction // Ensure that cookie is encrypted & https connection. Prod = true, dev/localhost is not an encrypted connection
+
+	// Set session in config to session we just initialized
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("can't create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting app o port %s", port)
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/kmulqueen/gobnb/models"
+	"github.com/kmulqueen/gobnb/pkg/config"
+	"github.com/kmulqueen/gobnb/pkg/render"
+)
+
+// Repo is the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Handler functions
+/*
+In order for a function to respond to a request from
+a web browser, it has to have 2 parameters.
+	1. Response Writer
+	2. Request
+*/
+// Home is the about page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	/* Everytime someone hits the home page, for that user's session
+	store the remoteIP as a string in the session. The key to look it up
+	is "remote_ip" */
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// Initializing data to pass to template
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// Send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{StringMap: stringMap})
+}
